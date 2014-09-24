568ab480c56ef4f02c0c5e4430936b55ad110794
Will Hopper <whopper@puppetlabs.com>
2014-09-24 00:30:20 +0000

    Add stubs for `cat` command implementation

---
+++ cat/lib/inc/cat.hpp
@@ -0,0 +1,24 @@
+/*
+ * cat.h
+ * Header for cat.cc
+ */
+
+#include <string>
+
+using namespace std;
+
+class cat
+{
+    public:
+        cat(string path);
+        int print_file_contents();
+
+    protected:
+        int get_file_contents();
+        bool check_file_exists();
+
+    private:
+        string filepath;
+        string contents;
+        bool is_valid_file;
+}
---
+++ cat/lib/src/cat-test.cc
@@ -0,0 +1,16 @@
+/*
+ * cat-test.cc
+ * A simple practice program to implement to `cat` Unix command.
+ * Uses the boost library.
+ * This file is a placeholder for the eventual wshell implementation, which
+ * will handle command execution.
+ */
+
+#include "../inc/cat.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    
+}
